cc66c9a8753c253b8b5414327c79048b03aa9743
Dhruv Aeron <dhruv.aeron3@gmail.com>
2022-10-30 22:22:28 +0530

    side-logos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { BrowserRouter , Routes, Route } from "react-router-dom";
-import Hero from "./Assets/hero";
-import Navbar from "./Assets/nav";
+// import { BrowserRouter , Routes, Route } from "react-router-dom";
+// import Hero from "./Assets/hero";
+// import Navbar from "./Assets/nav";
+import Home from "./Assets/home";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Hero />} ></Route>
-        </Routes>
-      </BrowserRouter>
+<Home/>
     </>
   );
 };
--- src/Assets/nav.js
+++ src/Assets/nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./css/nav.css";
 import logo from "./img/logo-2.png";
 
@@ -9,24 +9,26 @@ const Navbar = () => {
       <section className="fixed w-screen">
         <div className="flex navbar pl-8 pr-12 ">
           <div>
-            <Link to="/">
-              <img src={logo} alt="dhruv aeron logo" className="h-20" />
-            </Link>
+            <img
+              src={logo}
+              alt="dhruv aeron logo cursor-pointer"
+              className="h-20"
+            />
           </div>
           <div className="flex  items-center sfmono-med">
-            <div className="flex gap-2 text-xs px-5">
+            <div className="flex gap-2 text-xs px-5 cursor-pointer">
               <div className="text-green">01.</div>
               <div className="text-white hover:text-green">About</div>
             </div>
-            <div className="flex gap-2 text-xs px-5">
+            <div className="flex gap-2 text-xs px-5 cursor-pointer">
               <div className="text-green">02.</div>
               <div className="text-white hover:text-green">Experience</div>
             </div>
-            <div className="flex gap-2 text-xs px-5">
+            <div className="flex gap-2 text-xs px-5 cursor-pointer">
               <div className="text-green">03.</div>
               <div className="text-white hover:text-green">Work</div>
             </div>
-            <div className="flex gap-2 text-xs px-5">
+            <div className="flex gap-2 text-xs px-5 cursor-pointer">
               <div className="text-green">04.</div>
               <div className="text-white hover:text-green">Contact</div>
             </div>
