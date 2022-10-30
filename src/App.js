f01daf96f616bb4a14de08eaa2a07ada180479d5
Dhruv Aeron <dhruv.aeron3@gmail.com>
2022-10-30 19:02:00 +0530

    hero-done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import './App.css';
-
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import Hero from "./Assets/hero";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Hero />} ></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
---
+++ src/Assets/hero.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./css/hero.css"
+import photo from "./img/pic.png";
+const Hero = () => {
+  return (
+    <>
+      <section className="mt-48">
+        <div className="container px-56 ">
+          <div className="text-green text-base sfmono-sb">Hi, my name is </div>
+          <div className="text-llslate   calibre-sb text-8xl mt-8">
+            Dhruv Aeron.
+          </div>
+          <div className="text-slate text-7xl  calibre-sb">
+            I build things for the web.
+          </div>
+          <div className="text-slate text-xl w-7/12 leading-normal text-justify  calibre-reg mt-4">
+            I’m a undergard at USICT, New Delhi specializing in building (and
+            occasionally designing) Web experiences. Currently, I’m focused on
+            exploring new Technological Areas.
+          </div>
+          <div className="mt-12">
+            <a href={photo} download={photo} className="cv-a ">
+              <button type="submit" className="cv-btn sfmono-sb">
+                Resume!!
+              </button>
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero
